e13a924d7b36e5350219b20101314889146910aa
biaoye <biaoye@usc.edu>
2024-07-14 00:39:29 -0700

    update_dev_origin

--- api/main.py
+++ api/main.py
@@ -59,8 +59,8 @@ app.add_middleware(
     allow_origins=[
         "https://math-solver-frontend-five.vercel.app",
         "http://math-solver-frontend-five.vercel.app",
-        "https://mathsolver-frontend-dev.vercel.app/",
-        "http://mathsolver-frontend-dev.vercel.app/",
+        "https://mathsolver-frontend-dev.vercel.app",
+        "http://mathsolver-frontend-dev.vercel.app",
         "https://dev.mathsolver.top",
         "http://localhost:3000",
         "https://localhost:3000",
