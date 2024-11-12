292315072d8eb2fac7a87ecb01582aaccd0e4f87
Xuebin Dong <xuebindong@Xuebins-MacBook-Air.local>
2024-11-11 20:22:29 -0800

    Updates

--- dashboard/common/objects.py
+++ dashboard/common/objects.py
@@ -4,6 +4,6 @@ from src.users.supabase import UsersSupabase
 from src.users.users import User
 
 url = "https://stuabaechwhrzbjfddyq.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0dWFiYWVjaHdocnpiamZkZHlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NTA2NjcsImV4cCI6MjA0NjQyNjY2N30.yShFh5HH6H-1uKGRSZKJGujlmwPPAw54x-ZTLu1Ssow"
+key = ""
 user_services = UsersSupabase(url, key)
 users = User(user_services)
